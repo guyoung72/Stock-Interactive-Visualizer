26c27b2ef85c01428009b41aebae98111e624691
guyoung2 <guyoung2@illinois.edu>
2022-03-05 20:57:08 -0600

    [word_clouds.py] 파일명 변경
    [Visualization.py]
    x축 시간 조절 버튼 추가 (필요없으면 삭제해도 됩니다)
    참고사이트:
    https://medium.datadriveninvestor.com/how-to-generate-a-graph-for-a-stock-price-with-python-yahoo-finance-and-plotly-7f3a36f5ce7e

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # first_trading_proj
 
 This Github will include the following projects:
-1. Word bubbles showing current news key words for each NASDAQ stock
+1. Word clouds showing current news key words for each NASDAQ stock
 2. Trading bot (may need personal account)
--- visualization.py
+++ visualization.py
@@ -11,8 +11,21 @@ data = yf.download(tickers=ticker_input, period="3y", interval="1d")
 
 fig = go.Figure()
 fig.add_trace(go.Candlestick())
-fig.add_trace(go.Candlestick(open = data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'market data'))
+fig.add_trace(go.Candlestick(x=data.index, open = data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'market data'))
 
 fig.update_layout(title = ticker_input + ' share price', yaxis_title = 'Stock Price (USD)')
+
+# fig.update_xaxes(
+# rangeslider_visible=True,
+# rangeselector=dict(
+# buttons=list([
+# dict(count=15, label='15m', step="minute", stepmode="backward"),
+# dict(count=45, label='45m', step="minute", stepmode="backward"),
+# dict(count=1, label='1h', step="hour", stepmode="backward"),
+# dict(count=6, label='6h', step="hour", stepmode="backward"),
+# dict(step="all")
+# ])
+# )
+# )
 
 fig.show()

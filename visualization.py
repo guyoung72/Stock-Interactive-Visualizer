a7a742715a129ac331eb47e7e4a2a773f9390883
mbhang2 <mbhang2@illinois.edu>
2022-03-05 19:13:49 -0600

    Added user interaction program basics

--- visualization.py
+++ visualization.py
@@ -3,13 +3,16 @@ import pandas as pd
 import yfinance as yf
 import plotly.graph_objs as go
 
-# Test Case - SPY 5d5m
-data = yf.download(tickers="SPY", period="3y", interval="1d")
+# User Interaction program
+ticker_input = input("Stock Ticker: ")
+ticker_input = ticker_input.upper()
+
+data = yf.download(tickers=ticker_input, period="3y", interval="1d")
 
 fig = go.Figure()
 fig.add_trace(go.Candlestick())
 fig.add_trace(go.Candlestick(open = data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name = 'market data'))
 
-fig.update_layout(title = 'SPY share price', yaxis_title = 'Stock Price (USD)')
+fig.update_layout(title = ticker_input + ' share price', yaxis_title = 'Stock Price (USD)')
 
 fig.show()

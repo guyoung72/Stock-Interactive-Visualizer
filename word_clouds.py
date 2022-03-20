4fb654ba37e71bb42a698b4d346f9b56e99c58ec
guyoung2 <guyoung2@illinois.edu>
2022-03-20 01:48:42 -0500

    .

--- word_clouds.py
+++ word_clouds.py
@@ -1,38 +1,27 @@
-import requests
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-import csv
-import pandas as pd
+import requests
 
-# #change it to input format after finishing making the whole webscraper
-# page=urllib.request.urlopen("https://www.webull.com/newslist/nasdaq-msft")
-# # page=urllib.request.urlopen("https://www.investing.com/equities/microsoft-corp-commentary")
-# soup=bs(page,"html.parser")
-#
-# names=soup.body.findAll('div')
-# function_names=re.findall('wbus14.\w+',str(names))
-#
-# print(names)
+root="https://www.google.com/"
+link="https://www.google.com/search?q=samsung&newwindow=1&tbm=nws&sxsrf=APq-WBu72XeslioaVhmBnd0zw0ThgKeD_w:1647756625505&source=lnt&tbs=qdr:w&sa=X&ved=2ahUKEwi0qdvig9T2AhXWU80KHY4eDbkQpwV6BAgBEBw&biw=1920&bih=937&dpr=1"
 
-mostActiveStocksUrl = "https://www.nasdaq.com/market-activity/most-active"
-r = requests.get(mostActiveStocksUrl)
-data = r.text
-soup = BeautifulSoup(data)
-
-table = soup.find_all('div', attrs="class"&"genTable")
-all_rows = table[1].find_all('tr')
-
-symbols = []
-names = []
-last_sales = []
-change_nets = []
-share_volumes = []
-
-for row in all_rows:
-    cols=row.find_all('td')
-    if(len(cols)):
-        names.append(cols[1].text)
-        last_sales.append(cols[2].text)
-        change_nets.append(cols[3].text)
-        share_volumes.append(cols[4].text)
-
-data=pd.DataFrame({&quot;Names&quot;:names,&quot;Last Sale&quot;: last_sales,&quot;Chnange Net&quot;: change_nets,&quot;Share Volume&quot;: share_volumes})
+req=Request(link,headers={'User-Agent':'Mozilla/5.0'})
+webpage=urlopen(req).read()
+with requests.Session() as c:
+    soup=BeautifulSoup(webpage,'html5lib')
+    # print(soup)
+    for item in soup.find_all('div',attrs={'class':'ZINbbc luh4tb xpd O9g5cc uUPGi'}):
+        raw_link=(item.find('a',href=True)['href'])
+        link=raw_link.split("/url?q=")[1].split("&sa=U&")[0]
+        # print(item)
+        title=(item.find('div',attrs={'class':'BNeawe vvjwJb AP7Wnd'})).get_text()
+        desc=(item.find('div',attrs={'class':'BNeawe s3v9rd AP7Wnd'})).get_text()
+        time=desc.split("·")[0]
+        descript=desc.split("·")[1]
+        # print(title)
+        # print(time)
+        # print(descript)
+        # print(link)
+        document=open("data.csv","a")
+        document.write("{},{},{},{} \n".format(title, time, descript, link))
+        document.close()
